456d459a8aff862f6a20d55398925173d09f71dc
Miroslav Stoyanov <miroslav.stoyanov@androtech.com>
2017-12-22 13:38:49 +0200

    Added more services

---
+++ Bolt/Bolt.Services/Contracts/IOrdersService.cs
@@ -0,0 +1,19 @@
+﻿namespace Bolt.Services.Contracts
+{
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+
+    using Models;
+    using DTOs.Orders;
+
+    public interface IOrdersService
+    {
+        Task<int> ReOrder(int orderId);
+
+        Task<OrderStatus> GetOrderStatusAsync(int orderId);
+
+        Task<int> AddOrderAsync(CreateOrderDTO orderDTO);
+
+        Task<List<GetOrderDTO>> GetOrdersForUser(string userId, int numberOfOrdersToTake);
+    }
+}
