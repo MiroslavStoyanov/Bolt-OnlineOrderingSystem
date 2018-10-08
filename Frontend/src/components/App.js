b106a104999dab8db72d13cbb0390c07e61fe453
Miroslav Stoyanov <miro.stoyanov23@gmail.com>
2018-10-08 17:31:31 +0300

    Redid the App.js file

--- Frontend/src/components/App.js
+++ Frontend/src/components/App.js
@@ -1,12 +1,33 @@
-import React, {PropTypes} from 'react';
+import React, {PropTypes, Component} from 'react';
 import Header from './common/Header';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+import LoginPage from './common/LogInPage';
+import './App.css';
 
-class App extends React.Component {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loginPage: [],
+            uploadScreen: []
+        };
+    }
+
+    componentWillMount() {
+        let loginPage = [];
+        loginPage.push(<LoginPage parentContext={this} />);
+        this.setState({
+            loginPage: loginPage
+        });
+    }
+
     render() {
         return (
-            <div className="container-fluid">
+            <div className="App">
                 <Header />
-                {this.props.children}
+                {this.state.loginPage}
+                {this.state.uploadScreen}
             </div>
         );
     }
