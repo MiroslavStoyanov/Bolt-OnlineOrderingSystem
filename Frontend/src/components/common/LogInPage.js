2cae26bd5d269b38e646e1fd516fac87c69f58b8
Miroslav Stoyanov <miro.stoyanov23@gmail.com>
2018-10-08 16:27:32 +0300

    Created the registration and login pages

--- Frontend/src/components/common/LogInPage.js
+++ Frontend/src/components/common/LogInPage.js
@@ -1,13 +1,63 @@
-import React, {PropTypes} from 'react';
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import Login from '../login/Login';
+import Register from '../register/Register';
 
-class LogInPage extends React.Component {
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            loginscreen: [],
+            loginmessage: '',
+            buttonLabel: 'Register',
+            isLogin: true
+        };
+    }
+
+    componentWillMount() {
+        let loginScreen = [];
+
+        loginScreen.push(
+        <Login 
+            parentContext={this} 
+            appContext={this.props.parentContext}    
+        />);
+
+        const loginMessage = "Not registered yet. Register Now!";
+
+        this.setState({
+            loginScreen: loginScreen,
+            loginMessage: loginMessage
+        });
+    }
+
     render() {
         return (
-            <div>
-                <h1>Sign In / Register</h1>
+            <div className="loginscreen">
+            {this.state.loginscreen}
+                <div>
+                {this.state.loginmessage}
+                    <MuiThemeProvider>
+                        <div>
+                            <RaisedButton 
+                                label={this.state.buttonLabel} 
+                                primary 
+                                style={style} 
+                                onClick={(event) => this.handleClick(event)}
+                            />
+                        </div>
+                    </MuiThemeProvider>
+                </div>
             </div>
         );
     }
 }
 
-export default LogInPage;
+const style = {
+    margin: 15
+  };
+
+export default LoginPage;
--- Frontend/src/components/common/RegisterPage.js
+++ Frontend/src/components/common/RegisterPage.js
@@ -3,8 +3,24 @@ import React, {PropTypes} from 'react';
 class RegisterPage extends React.Component {
     render() {
         return (
-            <div>
+            <div className="container">
                 <h1>Register</h1>
+                <p>Please fill in this form to create an account.</p>
+                <br/>
+                <label htmlFor="username"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="username" required></input>
+                <br/>
+                <label htmlFor="email"><b>Email</b></label>
+                <input type="text" placeholder="Enter Email" name="email" required></input>
+                <br/>
+                <label htmlFor="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required></input>
+                <br/>
+                <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
+                <input type="password" placeholder="Repeat Password" name="psw-repeat" required></input>
+                <br/>
+                <p>By creating an account you agree to our <a href="#">Terms and Privacy</a>.</p>
+                <button type="submit" className="registerbtn">Register</button>
             </div>
         );
     }
